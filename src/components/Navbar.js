ff0c5ae247ad9b19586ec874ed14bfb6d9e37a29
sharad7s7 <sharad2014t@gmail.com>
2025-07-07 11:40:01 +0530

    GitHub Profile Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/sharad7s7/PortFolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
